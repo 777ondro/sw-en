693e3aa3ff3e6699a26545947a38759c9673ad18
SW.EN.OP <SW.EN.OP@46c3b7be-eb54-11de-8de6-035db03795fd>
2011-07-16 14:19:57 +0000

    kreslime v 3D kocku :-)

--- sw_en/sw_en_GUI/Window1.xaml.cs
+++ sw_en/sw_en_GUI/Window1.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Shapes;
 using System.Windows.Media.Media3D;
 using CENEX;
+using System.Windows.Media;
 
 namespace sw_en_GUI
 {
@@ -21,9 +22,58 @@ namespace sw_en_GUI
         public Window1()
         {
             InitializeComponent();
-            LookAt((PerspectiveCamera)viewPort1.Camera, new Point3D(0, 0, 0));
-            CConsoleHelper.Create();
-        }
+            //LookAt((PerspectiveCamera)viewPort1.Camera, new Point3D(0, 0, 0));
+            //CConsoleHelper.Create();
+            Model3DGroup models = new Model3DGroup();
+
+              Point3D p0 = new Point3D(-1, -1, -1);
+              Point3D p1 = new Point3D(1, -1, -1);
+              Point3D p2 = new Point3D(1, -1, 1);
+              Point3D p3 = new Point3D(-1, -1, 1);
+              Point3D p4 = new Point3D(-1, 1, -1);
+              Point3D p5 = new Point3D(1, 1, -1);
+              Point3D p6 = new Point3D(1, 1, 1);
+              Point3D p7 = new Point3D(-1, 1, 1);
+
+              models.Children.Add(CreateRectangle(p3, p2, p6, p7, Brushes.Red));  // front
+              models.Children.Add(CreateRectangle(p2, p1, p5, p6, Brushes.Red));  // right
+              models.Children.Add(CreateRectangle(p1, p0, p4, p5, Brushes.Red));  // back
+              models.Children.Add(CreateRectangle(p0, p3, p7, p4, Brushes.Red));  // left
+              models.Children.Add(CreateRectangle(p7, p6, p5, p4, Brushes.Red));  // top
+              models.Children.Add(CreateRectangle(p2, p3, p0, p1, Brushes.Red));  // bottom
+
+
+              ModelVisual3D visual = new ModelVisual3D();
+              visual.Content = models;
+
+              myViewport3D.Children.Add(visual);
+          }
+
+          public GeometryModel3D CreateRectangle(
+              Point3D point1, Point3D point2, 
+              Point3D point3, Point3D point4, 
+              Brush brush)
+          {
+              MeshGeometry3D mesh = new MeshGeometry3D();
+              mesh.Positions.Add(point1);
+              mesh.Positions.Add(point2);
+              mesh.Positions.Add(point3);
+              mesh.Positions.Add(point4);
+
+              mesh.TriangleIndices.Add(0);
+              mesh.TriangleIndices.Add(1);
+              mesh.TriangleIndices.Add(2);
+
+              mesh.TriangleIndices.Add(0);
+              mesh.TriangleIndices.Add(2);
+              mesh.TriangleIndices.Add(3);
+
+              return new GeometryModel3D(mesh, 
+                  new DiffuseMaterial(brush));
+          }
+
+
+
 
         private void LookAt(PerspectiveCamera camera, Point3D lookAtPoint)
         {
